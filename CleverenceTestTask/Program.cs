5c9d3ac5825b1e0237172413b870c4d650018fb1
streetkillahh <streetkillahh@mail.ru>
2025-05-05 17:36:57 +0500

    Add project files.

---
+++ CleverenceTestTask/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // 1-я задача
+
+
+        // 2-я задача
+        var tasks = new List<Task>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            tasks.Add(Task.Run(() => Console.WriteLine($"Read: {Server.GetCount()}")));
+            tasks.Add(Task.Run(() => Server.AddToCount(1)));
+        }
+
+        Task.WaitAll(tasks.ToArray());
+
+        Console.WriteLine($"Final Count: {Server.GetCount()}");
+
+        // 3-я задача
+
+    }
+}
---
+++ CleverenceTestTask/Task2/Server.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+public static class Server
+{
+    private static int _count = 0;
+
+    // Объект для управления доступом к _count
+    private static readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
+
+    // Чтение значения count
+    public static int GetCount()
+    {
+        _lock.EnterReadLock();
+        try
+        {
+            return _count;
+        }
+        finally
+        {
+            _lock.ExitReadLock();
+        }
+    }
+
+    // Добавление значения к count
+    public static void AddToCount(int value)
+    {
+        _lock.EnterWriteLock();
+        try
+        {
+            _count += value;
+        }
+        finally
+        {
+            _lock.ExitWriteLock();
+        }
+    }
+}
